3876717520a25b264e64d288f6f304ef89462f04
Mr-Biskit <dev@mrbiskit.xyz>
2024-01-10 11:09:11 +0100

    build(SvgDraw.tsx): positioning and animation functional

--- src/app/(user)/menu/page.tsx
+++ src/app/(user)/menu/page.tsx
@@ -4,10 +4,12 @@ import React from "react";
 
 function Menu() {
   return (
-    <div className="flex  flex-col items-center justify-center">
-      <h1 className="text-heading text-center text-[9rem] italic text-eggRed">
-        Menu
-      </h1>
+    <div className="flex  min-h-screen flex-col bg-eggWhite">
+      <div className="flex  flex-col items-center justify-center">
+        <h1 className="text-heading text-center text-[9rem] italic text-eggRed">
+          Menu
+        </h1>
+      </div>
       <SvgDraw />
     </div>
   );
--- src/components/SvgDrawing/SvgDraw.tsx
+++ src/components/SvgDrawing/SvgDraw.tsx
@@ -4,13 +4,13 @@ import { motion, useScroll } from "framer-motion";
 function SvgDraw() {
   const { scrollYProgress } = useScroll();
   return (
-    <div className="m-0 flex h-[300vh] bg-[#223C32]">
-      <div className=" h-full w-full  text-center">
+    <div className="relative m-0 h-[200vh] bg-[#223C32]">
+      <div className="absolute left-0 top-0 h-full w-full overflow-hidden px-4 text-center">
         <motion.svg
           viewBox="0 0 1293 972"
           fill="none"
           preserveAspectRatio="xMidYMax meet"
-          className="fixed left-0 top-0"
+          className="inline-block h-auto w-auto"
         >
           <motion.path
             style={{ pathLength: scrollYProgress }}
@@ -31,48 +31,12 @@ function SvgDraw() {
             fill="none"
           />
         </motion.svg>
-        <motion.svg
-          style={{ width: "80%", height: "80%" }}
-          viewBox="0 0 480 480"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <motion.path
-            // initial={{ pathLength: 0 }}
-            // animate={{ pathLength: 1 }}
-            // transition={{
-            //   duration: 2,
-            //   ease: "easeInOut",
-            //   repeat: Infinity,
-            //   repeatType: "loop",
-            //   repeatDelay: 1,
-            // }}
-            style={{ pathLength: scrollYProgress }}
-            stroke={"#09201C"}
-            strokeWidth={4}
-            strokeDasharray="0 1"
-            fill="none"
-            d="M415,275Q422,310,417.5,354Q413,398,378,423Q343,448,299,423Q255,398,227.5,389.5Q200,381,151,401.5Q102,422,86,383.5Q70,345,65,309.5Q60,274,78.5,243.5Q97,213,87.5,176.5Q78,140,107.5,122Q137,104,160.5,74Q184,44,222,33Q260,22,293.5,43.5Q327,65,362,81Q397,97,386,142.5Q375,188,391.5,214Q408,240,415,275Z"
-          />
-        </motion.svg>
       </div>
+      <section className="relative grid h-screen items-center justify-center text-[4rem]">
+        <h1 className=" text-eggWhite">Hello This is content</h1>
+      </section>
     </div>
   );
 }
 
 export default SvgDraw;
-
-// const draw = {
-//     hidden: { pathLength: 0, opacity: 0 },
-//     visible: (i: any) => {
-//       const delay = 1 + i * 0.5;
-//       return {
-//         pathLength: 1,
-//         opacity: 1,
-//         transition: {
-//           pathLength: { delay, type: "spring", duration: 1.5, bounce: 0 },
-//           opacity: { delay, duration: 0.01 },
-//         },
-//       };
-//     },
-//   };
-//   const { scrollYProgress } = useScroll();
